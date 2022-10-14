d3287882bb9a2fef707cbb3101a43a6011b36f73
Deposits <azadeh.miri@yahoo.com>
2022-10-14 21:27:33 +0330

    Initial commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,14 +5,9 @@ import org.example.controller.Controller;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
-        final String DIRECTORY = "C:/Users/Eniac/Desktop/Deposits.xml";
+public class App {
+    public static void main(String[] args) throws IOException, InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        final String DIRECTORY = "E:\\PROGRAMING\\JAVA\\firstProject\\src\\main\\resources\\Deposits.xml";
         Controller.depositController(DIRECTORY);
     }
 }
